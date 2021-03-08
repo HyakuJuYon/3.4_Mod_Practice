5507eaa5ad953b032c3c52655cf97ced8255f50a
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-09 02:24:19 +0300

    Changes_4

--- Program.cs
+++ Program.cs
@@ -2,6 +2,17 @@
 
 namespace _3._4_Mod_Pract
 {
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DaysOfWeek MyFavoriteDay;
+
+            MyFavoriteDay = DaysOfWeek.Thursday;
+
+            Console.WriteLine(MyFavoriteDay);
+        }
+    }
     enum DaysOfWeek : byte 
             {
                 Sunday = 1,
