9440455d63f5e8f2265b09e5c9b02bf93a9598d1
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-09 01:48:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3._4_Mod_Pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const byte Sunday = 1;
+
+            const byte Monday = 2; 
+
+            Console.ReadKey();
+        }
+    }
+}
