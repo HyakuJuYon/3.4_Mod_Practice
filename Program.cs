5fe7a6f7e70ca05d5faf1acd3b2a129c3ff45241
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-09 02:21:20 +0300

    Changes_3

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@ namespace _3._4_Mod_Pract
     enum DaysOfWeek : byte 
             {
                 Sunday = 1,
-                Monday = 2,
-                Tuesday = 3,
-                Wednesday = 4,
-                Thursday = 5,
-                Friday = 6,
-                Saturday = 7
+                Monday,
+                Tuesday,
+                Wednesday,
+                Thursday,
+                Friday,
+                Saturday
             }
 }
